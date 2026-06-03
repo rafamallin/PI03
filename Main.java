ae593186fee312dcd6f00f07c75b1c08a22906b4
Calebe Durães <calebe.duraes31@gmail.com>
2026-06-02 21:50:09 -0300

    Implementando Imovel.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import src.Usuario;
+
 public class Main {
     public static void main(String[] args){
         Usuario usuario = new Usuario();
--- src/Intervalo.java
+++ src/Intervalo.java
@@ -1,12 +1,26 @@
+package src;
 public class Intervalo {
     private double minimo;
     private double maximo;
 
+    //Construtor  
     public Intervalo(double min, double max){
         this.minimo = min;
         this.maximo = max;
     }
 
+    /*=========
+      GETTERS
+    =========*/
+    public double getMaximo() {
+        return maximo;
+    }
+
+    public double getMinimo() {
+        return minimo;
+    }
+
+    //Metodos
     public Boolean check_in_interval(double valor){
         if (valor < this.maximo && valor > this.minimo){
             return true;
